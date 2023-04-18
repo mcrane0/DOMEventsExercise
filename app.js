be5e086a06105a97767b9939e41e8ad5260359a5
Miles Crane <miles.crane@stu.barren.kyschools.us>
2023-04-18 10:51:20 -0500

    completed non-bonus DOM events exercise

--- app.js
+++ app.js
@@ -11,6 +11,24 @@
     clicked.
 */
 
+const button1 = document.querySelector('#one');
+
+button1.onclick = function (){
+    alert("You clicked the first button! Congrats!");
+}
+
+const h3 = document.querySelector('h3');
+
+h3.addEventListener('mouseover', function(){
+    alert("You hovered over the h3 element! Congrats!");
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(){
+    alert(form.elements.entry.value);
+});
+
 
 
 // BONUS
